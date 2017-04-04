f4591976a8089d25f11430fd7568d93fb1c94a16
Kang <ptnhuan@gmail.com>
2017-04-04 07:37:40 +0700

    init front package
    init front package

--- trunk/packages/front/src/routes.php
+++ trunk/packages/front/src/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+*VI
+*/
+
 Route::get('/trang-chu', [
             'as' => 'trang_chu',
             'uses' => 'Front\Controllers\FrontController@index'
@@ -21,3 +25,30 @@ Route::get('/gioi-thieu/id{$id}', [
             'as' => 'gioi_thieu',
             'uses' => 'Front\Controllers\FrontController@gioithieu'
         ]);
+		
+		/**
+*VI
+*/
+
+Route::get('/trang-chu', [
+            'as' => 'trang_chu',
+            'uses' => 'Front\Controllers\FrontController@index'
+        ]);
+Route::get('/gioi-thieu', [
+            'as' => 'gioi_thieu',
+            'uses' => 'Front\Controllers\FrontController@gioithieu'
+        ]);
+Route::get('/tin-tuc', [
+            'as' => 'tin_tuc',
+            'uses' => 'Front\Controllers\FrontController@tintuc'
+        ]);
+
+Route::get('/dich-vu', [
+            'as' => 'tin_tuc',
+            'uses' => 'Front\Controllers\FrontController@dichvu'
+        ]);
+Route::get('/gioi-thieu/id{$id}', [
+            'as' => 'gioi_thieu',
+            'uses' => 'Front\Controllers\FrontController@gioithieu'
+        ]);
+
